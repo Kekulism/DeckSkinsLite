ba06b66c9dbb633f3465adf70bbb2ec5fecb6373
Kekulism <33201764+Kekulism@users.noreply.github.com>
2025-02-04 17:59:44 -0500

    DeckSkinsLite Cleanup (Final Update)
    stop using this

--- DSLite.lua
+++ DSLite.lua
@@ -1,103 +1,45 @@
---- STEAMODDED HEADER
---- MOD_NAME: DeckSkinsLite
---- MOD_ID: DSplus
---- MOD_AUTHOR: [Keku]
---- MOD_DESCRIPTION: A compatibility mod that allows old DeckSkins+ skins to be loaded by Steammodded's DeckSkin object! This mod will find all .lua files inside the /skins/ directory in this mod's folder and load each of them as an additional option in the Customize Decks menu.
---- BADGE_COLOR: 52d49f
---- PRIORITY: 999999999999999
-----------------------------------------------
-------------MOD CODE -------------------------
-sendDebugMessage("Launching DeckSkinsLite")
-
 local current_mod = SMODS.current_mod
 local mod_path = SMODS.current_mod.path
 local usable_path = mod_path:match("Mods/[^/]+")
+local path_pattern_replace = usable_path:gsub("(%W)","%%%1")
 
-sendDebugMessage("Mod Path: " .. usable_path)
+local dsl = "DeckSkinsLite"
+local ace_two = { "Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
+local two_ace = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
+local vanilla_suits = {'Hearts', 'Clubs', 'Diamonds', 'Spades'}
 
-function recursiveEnumerate(folder)
-	local fileTree = ""
-	for _, file in ipairs(love.filesystem.getDirectoryItems(folder)) do
-		local path = folder .. "/" .. file
-		local info = love.filesystem.getInfo(path)
-		fileTree = fileTree .. "\n" .. path .. (info.type == "directory" and " (DIR)" or "")
-		if info.type == "directory" then
-			fileTree = fileTree .. recursiveEnumerate(path)
-		end
+local isAllSuits = function(suit)
+	if suit == "*" or suit:lower() == "a" or suit:lower() == "all" then return true else return false end
+end
+
+local SUITS = {}
+for k, v in pairs(SMODS.Suits) do
+	SUITS[v.card_key] = v.original_key
+end
+
+local suitConv = function(suit)
+	for k, v in pairs(SUITS) do
+		if k:lower() == suit:lower() or v:lower() == suit:lower() then return v end
 	end
-	return fileTree
+	return nil
 end
 
-local files = {}
-for s in recursiveEnumerate(usable_path .. "/skins"):gmatch("[^\r\n]+") do
-	files[#files + 1] = s:gsub(usable_path .. "/skins/", "")
+local RANKS = {}
+for k, v in pairs(SMODS.Ranks) do
+	RANKS[v.shorthand] = v.original_key
 end
-sendDebugMessage(tprint(files))
 
-local function findExistingSuit(smods_suit)
-	for suit, short in pairs(suits) do
-		if smods_suit.key == suit and smods_suit.card_key == short then
-			return true
-		end
+local rankConv = function(rank)
+	for k, v in pairs(RANKS) do
+		if k:lower() == rank:lower() or v:lower() == rank:lower() then return v end
 	end
-	return false
+	return nil
 end
-
-local suits = {
-	Hearts = "H",
-	Clubs = "C",
-	Diamonds = "D",
-	Spades = "S"
-}
-
-local loadCounts = {
-	Hearts = 0,
-	Clubs = 0,
-	Diamonds = 0,
-	Spades = 0
-}
 
 local LANGS = {}
 for lang, tbl in pairs(G.LANGUAGES) do
 	if not tbl.omit then
 		table.insert(LANGS, lang)
-	end
-end
-
-for _, smods_suit in ipairs(SMODS.Suits) do
-	if not findExistingSuit(smods_suit) then
-		suits[smods_suit.key] = smods_suit.card_key
-		loadCounts[smods_suit.key] = 0
-	end
-end
-
-local EXTRA_SKINS = {}
-for suit in pairs(suits) do
-	EXTRA_SKINS[suit] = {}
-end
-
-local function findExistingKey(inputString, tbl)
-	for key, value in pairs(tbl) do
-		if key == inputString then
-			return true
-		end
-	end
-	return false
-end
-
-function isInt(n)
-	return type(n) == "number" and n % 1 == 0
-end
-
-local function loadAtlas(id, texture, contrast, _px, _py)
-	if not findExistingKey(id.."_"..contrast, SMODS.Atlases) then
-		SMODS.Atlas{
-			key = id.."_"..contrast,
-			path = texture,
-			px = _px,
-			py = _py,
-			atlas_table = 'ASSET_ATLAS'
-		}
 	end
 end
 
@@ -128,175 +70,128 @@ local function makeLocTXT(skin)
 	return loc_txt
 end
 
-local function getIcon(texture, suit, suitIcons, allSuitIcons)
-	if allSuitIcons then
-		if type(texture) == 'string' then
-			local _x = 0
-			if suit == "Hearts" then _x = 0 elseif suit == "Diamonds" then _x = 1 elseif suit == "Clubs" then _x = 2 elseif suit == "Spades" then _x = 3 end
-			return texture, { x = _x, y = 0}
-		elseif type(texture) == 'table' then
-			for icon_suit, icon in pairs(texture) do
-				for _suit, _short in pairs(suits) do
-					if icon_suit:lower() == _suit:lower() or icon_suit:lower() == _short:lower() then
-						return icon, { x = 0, y = 0}
-					end
-				end
-			end
+local function reverse_table(table)
+	local reversed = {}
+	for i, v in ipairs(table) do
+		reversed[i] = rankConv(v)
+	end
+	return reversed
+end
+
+local function findExistingKey(inputString, tbl)
+	for key, value in pairs(tbl) do
+		if key == inputString then
+			return true
+		end
+	end
+	return false
+end
+
+local function loadAtlas(atlas)
+	if not findExistingKey(atlas.key, SMODS.Atlases) then
+		SMODS.Atlas(atlas)
+	end
+end
+
+local function loadOutdatedDeckSkin(atlases, deckskins)
+	for i, v in ipairs(atlases) do
+		loadAtlas(v)
+	end
+	for i, v in ipairs(deckskins) do
+		SMODS.DeckSkin(v)
+	end
+end
+
+local function assembleOutdatedDeckSkin(skin)
+	local atlases = {}
+	local deckskins = {}
+
+	if not skin.texture then
+		sendWarnMessage("Missing texture value for DeckSkin "..skin.id..". This DeckSkin will not be loaded.", dsl)
+		return
+	end
+
+	local loc
+	if skin.name then loc = makeLocTXT(skin) else loc = nil end
+	local allSuits = false
+	local suit = suitConv(skin.suit)
+	if suit == nil then
+		if isAllSuits(skin.suit) then
+			allSuits = true
+		else
+			sendWarnMessage("Invalid suit value for DeckSkin "..skin.id..". This DeckSkin will not be loaded.", dsl)
+			return
+		end
+	end
+	local cards = skin.cards or two_ace
+	for i, v in ipairs(cards) do
+		cards[i] = rankConv(v)
+	end
+
+	atlases[#atlases+1] = {
+		key = skin.id.."_1",
+		path = skin.texture,
+		px = 71,
+		py = 95,
+		atlas_table = 'ASSET_ATLAS',
+	}
+	if skin.highContrastTexture then
+		atlases[#atlases+1] = {
+			key = skin.id.."_2",
+			path = skin.highContrastTexture,
+			px = 71,
+			py = 95,
+			atlas_table = 'ASSET_ATLAS',
+		}
+	end
+
+	if allSuits then
+		for i, suit in ipairs(vanilla_suits) do
+			deckskins[i] = {}
+			deckskins[i].key = skin.id.."_"..suit
+			deckskins[i].suit = suit
+			deckskins[i].ranks = cards
+			deckskins[i].display_ranks = cards
+			deckskins[i].lc_atlas = skin.id.."_1"
+			deckskins[i].hc_atlas = skin.highContrastTexture ~= nil and skin.id.."_2" or skin.id.."_1"
+			deckskins[i].loc_txt = loc
+			deckskins[i].pos_style = "deck"
 		end
 	else
-		if type(suitIcons) == 'table' then
-			for i, icon_suit in ipairs(suitIcons) do
-				for _suit, _short in pairs(suits) do
-					if icon_suit:lower() == _suit:lower() or icon_suit:lower() == _short:lower() then
-						if type(texture) == 'string' then
-							return texture, { x = i-1, y = 0}
-						elseif type(texture) == 'table' then
-							if texture[_suit] then
-								return texture[_suit], { x = i-1, y = 0}
-							end
-						end
-					end
-				end
-			end
-		elseif type(suitIcons) == 'string' then
-			return suitIcons, { x = 0, y = 0}
+		deckskins[#deckskins+1] = {}
+		deckskins[#deckskins].key = skin.id
+		deckskins[#deckskins].suit = suit
+		deckskins[#deckskins].ranks = cards
+		deckskins[#deckskins].display_ranks = cards
+		deckskins[#deckskins].lc_atlas = skin.id.."_1"
+		deckskins[#deckskins].hc_atlas = skin.highContrastTexture ~= nil and skin.id.."_2" or skin.id.."_1"
+		deckskins[#deckskins].loc_txt = loc
+		if skin.cards == two_ace then
+			deckskins[#deckskins].pos_style = 'suit'
+		else
+			deckskins[#deckskins].pos_style = 'ranks'
 		end
 	end
+
+	loadOutdatedDeckSkin(atlases, deckskins)
 end
 
-local function getColor(skin_color, suit)
-	if type(skin_color) == "string" then
-		return skin_color
-	elseif type(skin_color) == "table" then
-		for suit_color, color in pairs(skin_color) do
-			for _suit, short in pairs(suits) do
-				if suit_color:lower() == _suit:lower() or suit_color:lower() == short:lower() then
-					if _suit == suit then
-						return skin_color[suit_color]
-					end
-				end
-			end
+function recursiveEnumerate(folder)
+	local fileTree = ""
+	for _, file in ipairs(love.filesystem.getDirectoryItems(folder)) do
+		local path = folder .. "/" .. file
+		local info = love.filesystem.getInfo(path)
+		fileTree = fileTree .. "\n" .. path .. (info.type == "directory" and " (DIR)" or "")
+		if info.type == "directory" then
+			fileTree = fileTree .. recursiveEnumerate(path)
 		end
 	end
+	return fileTree
 end
 
-local function loadSkin(skin, suit, short, allSuits, cards, twoAce, allSuitIcons, suitIcons)
-	if skin.texture then
-		local cardW, cardH = 71, 95
-		if skin.cardWidth then
-			if isInt(skin.cardWidth) then cardW = skin.cardWidth
-			else sendErrorMessage("[DeckSkinsLite] Invalid cardWidth value for skin "..skin.id.."! cardWidth must be an integer.") end
-		end
-		if skin.cardHeight then
-			if isInt(skin.cardHeight) then cardH = skin.cardHeight
-			else sendErrorMessage("[DeckSkinsLite] Invalid cardHeight value for skin "..skin.id.."! cardHeight must be an integer.") end
-		end
-		loadAtlas(skin.id, skin.texture, 1, cardW, cardH)
-		local style, loc, hc
-		local key = skin.id
-		local lcColor, hcColor, lcUI, hcUI, hcUI_atlas = nil, nil, nil, nil, nil
-		local uiPos = nil
-		if skin.highContrastTexture then
-			loadAtlas(skin.id, skin.highContrastTexture, 2, cardW, cardH)
-			hc = skin.id.."_2"
-		else
-			hc = skin.id.."_1"
-		end
-		if allSuits then
-			style = 'deck'
-			key = skin.id.."_"..short
-		else
-			if twoAce then
-				style = 'suit'
-			else
-				style = 'collab'
-			end
-		end
-		if skin.name then
-			loc = makeLocTXT(skin)
-		else
-			loc = nil
-		end
-		if skin.color then lcColor = getColor(skin.color, suit) end
-		if skin.highContrastColor then hcColor = getColor(skin.highContrastColor, suit) else hcColor = lcColor end
-		if skin.suitIcon and skin.suitIconTexture then
-			lcUI, uiPos = getIcon(skin.suitIconTexture, suit, suitIcons, allSuitIcons)
-			loadAtlas(skin.id.."_UI", lcUI, 1, 18, 18)
-			if skin.highContrastSuitIconTexture then
-				hcUI, uiPos = getIcon(skin.highContrastSuitIconTexture, suit, suitIcons, allSuitIcons)
-				loadAtlas(skin.id.."_UI", hcUI, 2, 18, 18)
-				hcUI_atlas = skin.id.."_UI_2"
-			else
-				hcUI = lcUI
-				hcUI_atlas = skin.id.."_UI_1"
-			end
-		end
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - key: "..key)
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - suit: "..suit)
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - cards:")
-		sendDebugMessage(tprint(cards))
-		if cardW ~= 71 then
-			sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - px: "..cardW)
-		end
-		if cardH ~= 95 then
-			sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - py: "..cardH)
-		end
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - lc_atlas: "..skin.id.."_1")
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - hc_atlas:"..hc)
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - loc_txt:")
-		sendDebugMessage(tprint(loc))
-		sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - style:"..style)
-		if lcColor then
-			sendDebugMessage("[DeckSkinsMinus] "..skin.id.." - lcColor:")
-			if type(lcColor) == 'table' then
-				sendDebugMessage(tprint(lcColor))
-			else
-				sendDebugMessage(lcColor)
-			end
-		end
-		if hcColor then
-			sendDebugMessage("[DeckSkinsMinus] "..skin.id.." hcColor:")
-			if type(hcColor) == 'table' then
-				sendDebugMessage(tprint(hcColor))
-			else
-				sendDebugMessage(hcColor)
-			end
-		end
-		if lcUI then
-			if type(lcUI) == 'table' then
-				sendDebugMessage(tprint(lcUI))
-			else
-				sendDebugMessage(lcUI)
-			end
-		end
-		if hcUI then
-			if type(hcUI) == 'table' then
-				sendDebugMessage(tprint(hcUI))
-			else
-				sendDebugMessage(hcUI)
-			end
-		end
-		if uiPos then
-			sendDebugMessage("[DeckSkinsMinus] "..skin.id.." uiPos: ")
-			sendDebugMessage(tprint(uiPos))
-		end
-		SMODS.DeckSkin{
-			key = key,
-			suit = suit,
-			ranks = cards,
-			hc_atlas = skin.id.."_1",
-			lc_atlas = hc,
-			loc_txt = loc,
-			posStyle = style,
-			lc_colour = lcColor,
-			hc_colour = hcColor,
-			lc_ui_atlas = skin.id.."_UI_1",
-			hc_ui_atlas = hcUI_atlas,
-			ui_pos = uiPos
-		}
-		loadCounts[suit] = loadCounts[suit] + 1
-		table.insert(EXTRA_SKINS[suit], skin.id)
-	end
+local files = {}
+for s in recursiveEnumerate(usable_path .. "/skins"):gmatch("[^\r\n]+") do
+	files[#files + 1] = s:gsub(path_pattern_replace .. "/skins/", "")
 end
 
 local SKINS = {}
@@ -315,11 +210,11 @@ for index, skin in ipairs(SKINS) do
 		if type(skin.priority) == 'string' then
 			skin.priority = tonumber(skin.priority)
 			if not skin.priority then
-				sendWarnMessage("[DeckSkinsMinus] Invalid string at priority value for "..skin.id..", could not convert! Defaulting priority to 1000.")
+				sendWarnMessage("Invalid string at priority value for "..skin.id..", could not convert! Defaulting priority to 1000", dsl)
 				skin.priority = 1000
 			end
 		elseif type(skin.priority) ~= 'number' then
-			sendWarnMessage("[DeckSkinsMinus] Invalid/Unknown value at priority value for "..skin.id.."! Defaulting priority to 1000.")
+			sendWarnMessage("Invalid/Unknown value at priority value for "..skin.id.."! Defaulting priority to 1000.", dsl)
 			skin.priority = 1000
 		end
 	else
@@ -336,54 +231,11 @@ table.sort(SKINS, function(a, b)
 	return a.original_index < b.original_index
 end)
 
-local function vanillaSuit(tbl, short)
-	for _, v in ipairs(tbl) do
-		if short:lower() == v:lower() then
-			return true
-		end
-	end
-	return false
+for _, skin in ipairs(SKINS) do
+	assembleOutdatedDeckSkin(skin)
 end
 
-local notLoadedFlag = false
-for _, skin in ipairs(SKINS) do
-	local defaultCards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
-	local defaultSuits = { "H", "D", "C", "S"}
-	local cardShorts = {
-		J = "Jack",
-		Q = "Queen",
-		K = "King",
-		A = "Ace"
-	}
-	local cards = skin.cards or defaultCards
-	local suitIcons, allSuitIcons = nil, nil
-	if skin.suitIcon then
-		allSuitIcons = skin.suit:lower() == "all" or skin.suit:lower() == "a" or skin.suit == "*" or false
-		if allSuitIcons then
-			suitIcons = defaultSuits
-		else
-			suitIcons = skin.suitIcon or defaultSuits
-		end
-	end
-	for i, card in ipairs(cards) do
-		local normalized = card:lower():gsub("^%l", string.upper)
-		cards[i] = cardShorts[card:sub(1, 1)] or normalized
-	end
-	local allSuits = skin.suit:lower() == "all" or skin.suit:lower() == "a" or skin.suit == "*"
-	local twoAce = cards == defaultCards
-	notLoadedFlag = true
-	for suit, short in pairs(suits) do
-		if (allSuits and vanillaSuit(defaultSuits, short)) or (skin.suit:lower() == short:lower() or skin.suit:lower() == suit:lower()) then
-			loadSkin(skin, suit, short, allSuits, cards, twoAce, allSuitIcons, suitIcons)
-			notLoadedFlag = false
-		end
-	end
-	if notLoadedFlag then
-		sendWarnMessage("[DeckSkinsLite] Missing or Invalid suit value at skin ".. skin.id.."! Skin was not loaded...")
-	end
-end
-sendInfoMessage("Successfully loaded "..loadCounts.Hearts+loadCounts.Clubs+loadCounts.Diamonds+loadCounts.Spades.." skins as SMODS Objects with DeckSkinsLite:")
-sendInfoMessage(tprint(EXTRA_SKINS))
+
 
 -------------------
 --- Credits Tab ---
@@ -433,6 +285,3 @@ SMODS.current_mod.credits_tab = function()
 		}
 	}
 end
-----------------------------------------------
-------------MOD CODE END----------------------
-----------------------------------------------
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+> [!CAUTION]
+> DEPRECATED. This mod is made for compatibility with an old format for DeckSkins that is no longer used. As such, it is no longer being updated and I will not be giving any more support for it in the Balatro discord. **It will still work**, but I do not reccomend using it. If you can, PLEASE avoid using this, and use the DeckSkin API built into SMODS instead. It may be a bit more complicated, but it provides a much better system with many new features and optimizations. [Check out the SMODS Wiki Page for it here](https://github.com/Steamodded/smods/wiki/SMODS.DeckSkin).
+
 # DeckSkinsLite
 
 A mod for Balatro that simplifies the creation of custom skins for your deck. Custom Deck skins are automatically loaded from the mod's skins folder into the game. DeckSkinsLite is backwards compatible with all skin add-ons created for DeckSkins+, and allows new modders who are inexperienced with LUA to easily make their own skins that can be loaded with DeckSkinsLite.
@@ -8,12 +11,10 @@ A mod for Balatro that simplifies the creation of custom skins for your deck. Cu
 
 This mod requires [Lovely](https://github.com/ethangreen-dev/lovely-injector) and the latest version **(Version 1.0.0~ALPHA-1030f or later)** of [Steammodded](https://github.com/Steamopollys/Steamodded). Without these dependencies, this mod will not function.
 
-To install DeckSkinsLite, download the contents of this repo as a zip file and extract it into your Balatro mods folder. 
-Inside the Balatro mods folder, make sure the folder you place the contents of the repo does not end with `-main`! Name the folder something like "DeckSkinsLite".
+To install DeckSkinsLite, download the contents of this repo as a zip file and extract it into your Balatro mods folder.
 
 ## Creating New Deck Skins
 
-Making an add-on for DeckSkinsLite is super easy!
 All you need is a small .lua file that contains a table of variables in the /skins/ folder, and a 2 textures in the /assets/ folder (a 1x and 2x version).
 Check the /.example/ folder for more details on how DeckSkinsLite add-ons are formatted, [or check the Documentation for more details!](https://github.com/Kekulism/DeckSkinsPlus/wiki/Skin-Documentation)
 
